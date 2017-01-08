98165b48bd69111007257d5804a600a6e24f1811
James Swineson <i@swineson.me>
2017-01-08 21:49:13 +0800

    Add type converter

--- collect.py
+++ collect.py
@@ -44,6 +44,25 @@ station_fields = frozenset((
 station_time = 'time_point'
 error_indicator = 'error'
 
+# convert data to specific type for InfluxDB line protocol
+string_wrapper = lambda s: "\"{}\"".format(s)
+int_wrapper = lambda s: "{}i".format(int(s))
+float_wrapper = lambda s: str(s)
+type_converters = {
+    'area': string_wrapper,
+    'position_name': string_wrapper,
+    'station_code': string_wrapper,
+    'so2': int_wrapper,
+    'pm10': int_wrapper,
+    'aqi': int_wrapper,
+    'o3': int_wrapper,
+    'quality': string_wrapper,
+    'pm2_5': int_wrapper,
+    'no2': int_wrapper,
+    'co': float_wrapper,
+    'primary_pollutant': string_wrapper,
+}
+
 # program
 for city in cities:
     payload['city'] = city
@@ -59,9 +78,9 @@ for city in cities:
             # get time
             timestamp = int(time.mktime(time.strptime(point[station_time], '%Y-%m-%dT%H:%M:%SZ'))  * 1000000000)
             # construct tags
-            tags = ','.join(['='.join([key, str(value)]) for key, value in {key:point[key] for key in station_tags}.items()])
+            tags = ','.join(['='.join([key, type_converters[key](value)]) for key, value in {key:point[key] for key in station_tags}.items()])
             # construct datapoints
-            data = ','.join(['='.join([key, str(value)]) for key, value in {key:point[key] for key in station_fields}.items()])
+            data = ','.join(['='.join([key, type_converters[key](value)]) for key, value in {key:point[key] for key in station_fields}.items()])
             # print as InfluxDB line protocol
             # https://docs.influxdata.com/influxdb/v1.1/write_protocols/line_protocol_reference/
             infline = "{},{} {} {}".format(measurement_name, tags, data, timestamp)
