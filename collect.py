7e1bae3fbe012c23b3ae2d196095c0874f07e92d
James Swineson <i@swineson.me>
2017-01-08 21:12:23 +0800

    request multiple cities (since public apikey allows no full query)

--- collect.py
+++ collect.py
@@ -3,12 +3,22 @@
 import requests
 import pprint
 import time
+import sys
 
 # config
+cities = frozenset((
+    'hangzhou',
+    'shijiazhuang',
+    'qingdao',
+    'ningbo',
+    'beijing',
+    'shanghai',
+    'xian',
+))
 endpoint = 'http://www.pm25.in/api/querys/aqi_details.json'
 payload = {
     'token': '5j1znBVAsnSf5xQyNQyq',
-    'city': 'hangzhou',
+    'city': '',
     'avg': 'false',
 }
 headers = {
@@ -32,18 +42,27 @@ station_fields = frozenset((
     'so2',
 ))
 station_time = 'time_point'
+error_indicator = 'error'
 
 # program
-r = requests.get(endpoint, params=payload, headers=headers)
-# pprint.PrettyPrinter().pprint(r.json())
-for point in r.json():
-    # get time
-    timestamp = int(time.mktime(time.strptime(point[station_time], '%Y-%m-%dT%H:%M:%SZ'))  * 1000000000)
-    # construct tags
-    tags = ','.join(['='.join([key, str(value)]) for key, value in {key:point[key] for key in station_tags}.items()])
-    # construct datapoints
-    data = ','.join(['='.join([key, str(value)]) for key, value in {key:point[key] for key in station_fields}.items()])
-    # print as InfluxDB line protocol
-    # https://docs.influxdata.com/influxdb/v1.1/write_protocols/line_protocol_reference/
-    infline = "{},{} {} {}".format(measurement_name, tags, data, timestamp)
-    print(infline)
+for city in cities:
+    payload['city'] = city
+    r = requests.get(endpoint, params=payload, headers=headers)
+    j = r.json()
+    if r.status_code != requests.codes.ok or error_indicator in j:
+        # got some error
+        print("Request failed for city: {}".format(city))
+        print("Response code: {}".format(r.status_code))
+        pprint.PrettyPrinter(indent=4, stream=sys.stderr).pprint(j)
+    else:
+        for point in j:
+            # get time
+            timestamp = int(time.mktime(time.strptime(point[station_time], '%Y-%m-%dT%H:%M:%SZ'))  * 1000000000)
+            # construct tags
+            tags = ','.join(['='.join([key, str(value)]) for key, value in {key:point[key] for key in station_tags}.items()])
+            # construct datapoints
+            data = ','.join(['='.join([key, str(value)]) for key, value in {key:point[key] for key in station_fields}.items()])
+            # print as InfluxDB line protocol
+            # https://docs.influxdata.com/influxdb/v1.1/write_protocols/line_protocol_reference/
+            infline = "{},{} {} {}".format(measurement_name, tags, data, timestamp)
+            print(infline)
